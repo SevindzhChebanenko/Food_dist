0bb73347a562aff0bd3e359e1320513dbf88b9b7
Sevindzh Chebanenko <misschebanenko@gmail.com>
2023-01-24 22:02:37 +0300

    second debag

--- js/script.js
+++ js/script.js
@@ -40,50 +40,106 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
-    //Modal
+
+
+const deadline = '2023-03-07'
+
+function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds
+    const t = Date.parse(endtime) - Date.parse(new Date())
+
+    if(t <= 0) {
+        days = 0
+        hours = 0
+        minutes = 0
+        seconds = 0
+    }else {
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / (1000 / 60) % 60)),
+        seconds = Math.floor((t / 1000) % 60)
+    }
+
+    return {'total': t, days, hours, minutes, seconds}
+}
+
+function getZero(num) {
+    if(num >=0 && num < 10) {
+        return `0${num}`
+    }else {
+        return num
+    }
+}
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000)
+
+updateClock()
+
+function updateClock() {
+    const t = getTimeRemaining(endtime)
+    days.innerHTML = getZero(t.days)
+    hours.innerHTML = getZero(t.hours)
+    minutes.innerHTML = getZero(t.minutes)
+    seconds.innerHTML = getZero(t.seconds)
+
+        if(t.total <= 0) {
+            clearInterval(timeInterval)
+        }
+    }
+}
+
+setClock('.timer', deadline)
+
+//Modal
 
 const modalTrigger = document.querySelectorAll('[data-modal]'),
-modal = document.querySelector('.modal'),
-modalCloseBtn = document.querySelector('[data-close]')
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]')
 
 function openModal() {
-modal.classList.add('show')
-modal.classList.remove('hide')
-document.body.style.overflow = 'hidden'
-clearInterval(modalTimerId)
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+    document.body.style.overflow = 'hidden'
+    clearInterval(modalTimerId)
 }
 
 modalTrigger.forEach(btn => {
-btn.addEventListener('click', openModal)
+    btn.addEventListener('click', openModal)
 })
 
 function closeModal() {
-modal.classList.add('hide')
-modal.classList.remove('show')
-document.body.style.overflow = ''
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+    document.body.style.overflow = ''
 }
 
 modalCloseBtn.addEventListener('click', closeModal)
 
 modal.addEventListener('click', (e) => {
-if (e.target === modal) {
-  closeModal()
-}
+    if (e.target === modal) {
+        closeModal()
+    }
 })
 
 document.addEventListener('keydown', (e) => {
-if(e.code === "Escape" && modal.classList.contains('show')) {
-  closeModal()
-}
+    if(e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal()
+    }
 })
 
 const modalTimerId = setTimeout(openModal, 5000)
 
 function endOfPage() {
-if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
-  openModal()
-}
-window.removeEventListener('scroll', endOfPage)
+    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+        openModal()
+    }
+    window.removeEventListener('scroll', endOfPage)
 } 
 
 window.addEventListener('scroll', endOfPage)
